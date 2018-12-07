3e143650612f5af1f5193dc054e34226b41fc19f
Marcin Ziemiński <ziemin@apache.org>
2018-12-07 19:50:01 +0100

    Work on graph

---
+++ optimizer/include/polly_ml_optimizer/graph.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "polly_ml_optimizer/scop.hpp"
+
+#include <boost/graph/adjacency_list.hpp>
+
+
+namespace pollyML {
+
+  using Graph = boost::adjacency_list<>;
+  
+} // namespace pollyML
